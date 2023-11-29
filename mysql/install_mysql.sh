4112c7efbbf89d518f5874b835fc992d33dc29ae
Sanghoon Lee <gcccompil3r@gmail.com>
2023-11-29 10:55:22 +0900

    update: [이상훈] mysql 설치 자동화 [DOI-12]

--- mysql/install_mysql.sh
+++ mysql/install_mysql.sh
@@ -2,8 +2,44 @@
 
 sudo apt update
 sudo apt install -y mysql-server
+sudo apt-get install -y expect
 
-sudo mysql_secure_installation
+set mysql_root_password "your_mysql_root_password"
+
+spawn sudo -S mysql_secure_installation
+
+expect {
+    "Password:" {
+        send "$mysql_root_password\r"
+        exp_continue
+    }
+    "Press y|Y for Yes, any other key for No:" {
+        send "n\r"
+        exp_continue
+    }
+    "Press y|Y for Yes, N|n for No:" {
+         send "n\r"
+	 exp_continue
+    }
+    "Remove anonymous users?" {
+         send "y\r"
+	 exp_continue
+    }
+    "Disallow root login remotely?" {
+         send "y\r"
+	 exp_continue
+    }
+    "Remove test database and access to it?" {
+         send "y\r"
+	 exp_continue
+    }
+    "Reload privilege tables now?" {
+         send "y\r"
+	 exp_continue
+    }
+}
+
+expect eof
 
 sudo systemctl start mysql
 sudo systemctl enable mysql
