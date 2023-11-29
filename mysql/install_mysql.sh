0c7ca0c54e7c2ce8a527ed6ce3e750ae90813e23
Sanghoon Lee <gcccompil3r@gmail.com>
2023-11-29 11:19:13 +0900

    fix: [이상훈] 쉘 스크립트 설정 수정 [DOI-12]

--- mysql/install_mysql.sh
+++ mysql/install_mysql.sh
@@ -4,42 +4,23 @@ sudo apt update
 sudo apt install -y mysql-server
 sudo apt-get install -y expect
 
-set mysql_root_password "your_mysql_root_password"
+read -s -p "Enter MySQL root password: " mysql_root_password
+echo
 
+expect <<EOF
+set timeout 0.1
 spawn sudo -S mysql_secure_installation
-
-expect {
-    "Password:" {
-        send "$mysql_root_password\r"
-        exp_continue
-    }
-    "Press y|Y for Yes, any other key for No:" {
-        send "n\r"
-        exp_continue
-    }
-    "Press y|Y for Yes, N|n for No:" {
-         send "n\r"
-	 exp_continue
-    }
-    "Remove anonymous users?" {
-         send "y\r"
-	 exp_continue
-    }
-    "Disallow root login remotely?" {
-         send "y\r"
-	 exp_continue
-    }
-    "Remove test database and access to it?" {
-         send "y\r"
-	 exp_continue
-    }
-    "Reload privilege tables now?" {
-         send "y\r"
-	 exp_continue
-    }
-}
-
+expect "Password:"
+log_user 0  ;# Disable logging to stdout
+send -- "$mysql_root_password\r"
+expect "Disallow root login remotely?"
+send "y\r"
+expect "Remove test database and access to it?"
+send "y\r"
+expect "Reload privilege tables now?"
+send "y\r"
 expect eof
+EOF
 
 sudo systemctl start mysql
 sudo systemctl enable mysql
