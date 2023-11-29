7a91fa17c6cfae6bed96692d28c144075159a0b4
Sanghoon Lee <gcccompil3r@gmail.com>
2023-11-29 10:59:30 +0900

    fix: [이상훈] 쉘 스크립트 설정 수정 [DOI-4]

--- mysql/install_mysql.sh
+++ mysql/install_mysql.sh
@@ -4,42 +4,26 @@ sudo apt update
 sudo apt install -y mysql-server
 sudo apt-get install -y expect
 
-set mysql_root_password "your_mysql_root_password"
+mysql_root_password="your_password"
 
+expect <<EOF
 spawn sudo -S mysql_secure_installation
-
-expect {
-    "Password:" {
-        send "$mysql_root_password\r"
-        exp_continue
-    }
-    "Press y|Y for Yes, any other key for No:" {
-        send "n\r"
-        exp_continue
-    }
-    "Press y|Y for Yes, N|n for No:" {
-         send "n\r"
-	 exp_continue
-    }
-    "Remove anonymous users?" {
-         send "y\r"
-	 exp_continue
-    }
-    "Disallow root login remotely?" {
-         send "y\r"
-	 exp_continue
-    }
-    "Remove test database and access to it?" {
-         send "y\r"
-	 exp_continue
-    }
-    "Reload privilege tables now?" {
-         send "y\r"
-	 exp_continue
-    }
-}
-
+expect "Password:"
+send "$mysql_root_password\r"
+expect "Press y|Y for Yes, any other key for No:"
+send "n\r"
+expect "Press y|Y for Yes, N|n for No:"
+send "n\r"
+expect "Remove anonymous users?"
+send "y\r"
+expect "Disallow root login remotely?"
+send "y\r"
+expect "Remove test database and access to it?"
+send "y\r"
+expect "Reload privilege tables now?"
+send "y\r"
 expect eof
+EOF
 
 sudo systemctl start mysql
 sudo systemctl enable mysql
